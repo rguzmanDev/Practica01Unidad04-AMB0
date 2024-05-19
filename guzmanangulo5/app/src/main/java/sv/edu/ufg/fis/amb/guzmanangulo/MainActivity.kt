2e0c7abc6d774b884ba21eb6f074d02282b8817d
rguzmanDev <rguzman.dev@outlook.com>
2024-05-18 23:35:38 -0600

    test

---
+++ guzmanangulo5/app/src/main/java/sv/edu/ufg/fis/amb/guzmanangulo/MainActivity.kt
@@ -0,0 +1,114 @@
+package sv.edu.ufg.fis.amb.guzmanangulo
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.os.Environment
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var editText: EditText
+    private lateinit var buttonSave: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        editText = findViewById(R.id.editText)
+        buttonSave = findViewById(R.id.buttonSave)
+
+        buttonSave.setOnClickListener {
+            val text = editText.text.toString()
+            if (text.isNotEmpty()) {
+                saveToInternalFile(text)
+                if (isExternalStorageWritable() && checkPermission()) {
+                    saveToExternalFile(text)
+                }
+
+                // Verificar si el archivo se guardó en la memoria interna
+                val fileNameInternal = "escritura_de_campo_interno.txt"
+                val fileInternal = File(filesDir, fileNameInternal)
+                if (fileInternal.exists()) {
+                    Log.d("Archivo Interno", "Existe en memoria interna")
+                } else {
+                    Log.d("Archivo Interno", "No existe en memoria interna")
+                }
+
+                // Verificar si el archivo se guardó en la memoria externa
+                val fileNameExternal = "escritura_de_campo_externo.txt"
+                val fileExternal = File(getExternalFilesDir(null), fileNameExternal)
+                if (fileExternal.exists()) {
+                    Log.d("Archivo Externo", "Existe en memoria externa")
+                } else {
+                    Log.d("Archivo Externo", "No existe en memoria externa")
+                }
+            } else {
+                Toast.makeText(this, "Ingrese un texto antes de guardar", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun saveToInternalFile(data: String) {
+        val fileName = "escritura_de_campo_interno.txt"
+        val file = File(filesDir, fileName)
+        try {
+            FileOutputStream(file, true).use { fos ->
+                fos.write((data + "\n").toByteArray())
+            }
+            editText.setText("")
+            Toast.makeText(this, "Guardado en memoria interna", Toast.LENGTH_SHORT).show()
+        } catch (e: IOException) {
+            e.printStackTrace()
+            Toast.makeText(this, "Error al guardar en memoria interna", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun saveToExternalFile(data: String) {
+        val fileName = "escritura_de_campo_externo.txt"
+        val file = File(getExternalFilesDir(null), fileName)
+        try {
+            FileOutputStream(file, true).use { fos ->
+                fos.write((data + "\n").toByteArray())
+            }
+            Toast.makeText(this, "Guardado en memoria externa", Toast.LENGTH_SHORT).show()
+        } catch (e: IOException) {
+            e.printStackTrace()
+            Toast.makeText(this, "Error al guardar en memoria externa", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun isExternalStorageWritable(): Boolean {
+        return Environment.getExternalStorageState() == Environment.MEDIA_MOUNTED
+    }
+
+    private fun checkPermission(): Boolean {
+        val permission = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        return if (permission != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), 1)
+            false
+        } else {
+            true
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 1) {
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                buttonSave.performClick()
+            } else {
+                Toast.makeText(this, "Permiso denegado", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
